5b293193f430ebd9fcb821093b0cc9b223d7008e
awl <bitb@awl.su>
2024-08-06 14:31:13 +0300

    Update dependency management and requirements
    Move requirements to requirements.txt and update dependency versions. Read dependencies from requirements.txt in setup.py for better maintainability.

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ with open("README.md", "r") as file:
     description = file.read()
     file.close()
 
+with open('requirements.txt') as f:
+    requirements = [req for req in f.read().splitlines() if req]
+
 setuptools.setup(
     name="eosapi",
     version="1.0.2",
@@ -15,7 +18,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/encoderlee/eosapi",
     packages=["eosapi"],
-    install_requires=["requests", "cryptos", "base58"],
+    install_requires=requirements,
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
