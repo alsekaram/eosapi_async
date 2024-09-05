6549c6b01ac3e3670eeeb3d9113677a2e7ecbc59
I-am-Vasya <github@awl.su>
2024-09-05 13:24:16 +0300

    Bump version to 1.1
    Updated setup.py and README.md to reflect the new version 1.1. This includes updating the version badge in README for consistency.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="eosapi-async",
-    version="1.0.4",
+    version="1.1",
     author="encoderlee",
     author_email="encoderlee@gmail.com",
     description="a simple, high-level and lightweight eosio sdk write by python with async support",
