0893219f9f2b4b2e252ed89f5bfd9ad98cb507f8
alsekaram <git@awl.su>
2024-09-15 16:18:07 +0300

    Update version to 1.2.4
    Bump the package version from 1.2.3 to 1.2.4 in setup.py and README.md to reflect recent changes and improvements.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="eosapi-async",
-    version="1.2.3",
+    version="1.2.4",
     author="encoderlee",
     author_email="encoderlee@gmail.com",
     description="a simple, high-level and lightweight eosio sdk write by python with async support",
