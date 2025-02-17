1385df851cd3684d041afadba289263ac5ff17a0
alsekaram <git@awl.su>
2025-02-17 18:55:48 +0300

    Update dependencies and bump version to 2.0.3
    Revised dependency list to focus on key requirements, simplifying setup.py. Dropped unnecessary or overly specific dependencies and updated version to 2.0.3 to reflect these changes.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setuptools.setup(
     name="eosapi-async",
-    version="2.0.2",  # Major version bump due to significant changes
+    version="2.0.3",  # Major version bump due to significant changes
     author="alsekaram",
     author_email="git@awl.su",
     description="EOS API async client with modern Python support",
@@ -22,30 +22,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/alsekaram/eosapi_async",
     install_requires=[
-        "aiohappyeyeballs==2.3.4",
-        "aiohttp==3.10.1",
-        "aiorpcX==0.23.1",
-        "aiosignal==1.3.1",
-        "attrs==24.1.0",
-        "base58>=2.1.1",
-        "certifi==2024.7.4",
-        "charset-normalizer==3.3.2",
+        "aiohttp>=3.8.0",
+        "requests>=2.26.0",
         "cryptos==2.0.9",
-        "frozenlist==1.4.1",
-        "idna==3.7",
-        "janus==1.0.0",
-        "multidict==6.0.5",
-        "packaging==24.1",
-        "pbkdf2==1.3",
-        "pycryptodomex==3.20.0",
-        "requests==2.32.3",
-        "typing_extensions==4.12.2",
-        "urllib3==2.2.2",
-        "yarl==1.9.4",
-        "pydantic~=2.8.2",
-        "antelopy~=0.2.0",
-        "setuptools~=72.1.0",
-        "cachetools~=5.5.0",
+        "base58==2.1.1",
+        "cachetools==5.5.1",
+        "pydantic==2.10.6",
+        "antelopy==0.2.0",
     ],  # Вместо чтения из requirements.txt указываем зависимости прямо здесь
     license="MIT",
     classifiers=[
